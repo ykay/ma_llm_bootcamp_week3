17aa62d4610c8728d402a7fe8eacace8487d550a
Yuichi Kuroda <yuichi.kuroda@gmail.com>
2024-09-27 23:38:40 -0700

    Add Milestone 5
    - Inserted whole User / Assistant conversation history in function_call
    AI context so it has the entire context to provide the most accurate
    function call along with the required parameters. The context from both sides
    is required because sometimes, the Assistant could make decisions like
    choosing a random movie, so that information would be required when the
    user proceeds with saying something like, "Buy a ticket now!" without providing
    the movie, showtime, etc.

--- app.py
+++ app.py
@@ -21,60 +21,59 @@ gen_kwargs = {
     "max_tokens": 500
 }
 
+function_signatures = {
+    "get_now_playing_movies()",
+    "get_showtimes(title, location)",
+    "get_reviews(movie_id)",
+    "buy_ticket(theater, movie, showtime)",
+    "confirm_ticket_purchase(theater, movie, showtime)",
+    "callback()"
+}
+
 SYSTEM_PROMPT = """
 You are a movie guru. You don't provide awkward qualifiers like, "According to TMDB API..." because no one talks like that and you should speak as if you already know what you know.
 """
 
-FUNCTION_SYSTEM_PROMPT = """\
+FUNCTION_SYSTEM_PROMPT = f"""\
 You provide information about movies. When you don't have the required information, you need to provide the appropriate function to call based on the user’s needs. When a function needs to be called, respond with one of the following function names:
 
-get_now_playing_movies()
-get_showtimes(title, location)
-get_reviews(movie_id)
-NONE
+{function_signatures}
 
 Make sure to replace parameters with the appropriate values. For example, if the user asks for showtimes for the movie "The Batman" in "New York", the response should be in the following:
 
-{
+{{
     "functions": ["get_showtimes('The Batman', 'New York')"]
-}
+}}
 
 If a function needs to be called, the response should only contain JSON data with the function name. For example, if the user asks for now playing movies, the response should be in the following:
-{
+{{
     "functions": ["get_now_playing_movies()"]
-}
+}}
 
 If there are multiple functions to call, the response should contain multiple function names in the list:
-{
+{{
     "functions": ["get_now_playing_movies()", "get_showtimes('The Batman', '95112')"]
-}
+}}
 
 If multiple functions need to be called, but more information is required first, append the callback to request the information. For example, if the user asks for now playing movies and showtimes for any random movie, append the callback function to request the information first:
-{
+{{
     "functions": ["get_now_playing_movies()", "callback()"]
-}
+}}
 
 You can also request a specific piece of information. For example, if the user asks for showtimes for a movie, but the location is missing, you can request the location first:
-{
+{{
     "functions": ["get_showtimes('The Batman', 'callback()')"]
-}
+}}
 
 Then, when the required information is provided, the follow-up response should call the next function with the required information. Remember to remove 'get_now_playing_movies()' from the list of functions to call once the information is provided and the next function(s) are being decided:
-{
+{{
     "functions": ["get_showtimes('The Batman', '95112')"]
-}
+}}
 
 If there is no appropriate function to call, "functions" should be set to an empty array (i.e., []).
 """
 
 function_call_history = [{"role": "system", "content": FUNCTION_SYSTEM_PROMPT}]
-
-function_signatures = {
-    "get_now_playing_movies()",
-    "get_showtimes(title, location)",
-    "get_reviews(movie_id)",
-    "callback()"
-}
 
 def parse_missing_info(functions):
     context = ""
@@ -130,6 +129,25 @@ async def process_function_call_response(completion):
             print("Calling get_reviews()")
             print("Movie ID: ", movie_id)
             context += movie_functions.get_reviews(movie_id)
+        elif func_name == "buy_ticket":
+            theater = params[0]
+            movie = params[1]
+            showtime = params[2]
+            print("Calling buy_ticket(); But confirming first.")
+            print("Theater: ", theater)
+            print("Movie: ", movie)
+            print("Showtime: ", showtime)
+            context += f"Ask the user if they really want to buy the ticket for {movie} at {theater} on {showtime}. If they confirm, call confirm_ticket_purchase()."
+        elif func_name == "confirm_ticket_purchase":
+            theater = params[0]
+            movie = params[1]
+            showtime = params[2]
+            print("Calling confirm_ticket_purchase()")
+            print("Theater: ", theater)
+            print("Movie: ", movie)
+            print("Showtime: ", showtime)
+            context += movie_functions.buy_ticket(theater, movie, showtime)
+            context += "Just pretend you bought a ticket." # Without this, the assistant responds that it can't buy tickets.
         elif func_name == "callback":
             if context:
                 print("Invoking callback with additional context.")
@@ -143,7 +161,7 @@ async def process_function_call_response(completion):
 
 async def function_calling(client, message_history):
     # Append the last message from the user
-    function_call_history.append({"role": "user", "content": message_history[-1]["content"]})
+    function_call_history.append({"role": "system", "content": f"Conversation Between User and Assistant: {message_history}"})
     completion = await client.chat.completions.create(messages=function_call_history, **gen_kwargs)
     
     try:
